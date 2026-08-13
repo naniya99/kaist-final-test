609934a00c0e3356a7b9e25871cad347c4697659
naniya99 <naniya99@github.com>
2026-08-13 10:20:37 +0900

    Add Azure key

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3348175798" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2504146257" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
